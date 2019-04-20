340290453a7ba8ddd51414634c5ffb679191d996
Alexander Makarov <sam@rmcreative.ru>
2019-04-20 16:18:34 +0300

    state service interfaces

--- Controllers/StateController.cs
+++ Controllers/StateController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using simulator_backend.Domain.Models;
+using simulator_backend.Domain.Service;
 
 namespace simulator_backend.Controllers
 {
@@ -10,11 +12,18 @@ namespace simulator_backend.Controllers
     [ApiController]
     public class StateController : ControllerBase
     {
+        private StateService Service;
+
+        public StateController(StateService Service)
+        {
+            this.Service = Service;   
+        }
+
         // GET api/state
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<State> Get()
         {
-            return new string[] { "value1", "value2" };
+            return this.Service.getCurrent();
         }
 
         // POST api/state
@@ -22,7 +31,8 @@ namespace simulator_backend.Controllers
         public void Post([FromBody] string value)
         {
             // TODO: deserialize JSON
-            // 
+            // instantiate post fields and transition object
+            this.Service.transit();
         }
     }
 }
---
+++ Domain/Services/StateService.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using simulator_backend.Domain.Models;
+
+namespace simulator_backend.Domain.Service
+{
+    public class StateService
+    {
+        public State getCurrent()
+        {
+            
+        }
+
+        public void transit(string TargetState, IList<Field> fields)
+        {
+
+        }
+    }
+}
