66be3cd7595580d8aba757a8c1c7762cba9ff2e1
Alexander Makarov <sam@rmcreative.ru>
2019-04-20 15:39:39 +0300

    models progress

---
+++ Domain/Models/FinishState.cs
@@ -0,0 +1,22 @@
+namespace simulator_backend.Domain.Models
+{
+    public class FinishState : State
+    {
+        private string Name;
+
+        public FinishState(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string getName()
+        {
+            return this.Name;
+        }
+
+        public string getType()
+        {
+            return "finish";
+        }
+    }
+}
--- Domain/Models/RegularState.cs
+++ Domain/Models/RegularState.cs
@@ -2,16 +2,36 @@ using System.Collections.Generic;
 
 namespace simulator_backend.Domain.Models
 {
-    public class State
+    public class RegularState : State
     {
-        public string Name { get; set; }
+        private string Name;
+        private string Content;
 
-        public string Type { get; } = "regular";
+        private Achievement achievement;
+
+        public RegularState(string Name, string Content)
+        {
+            this.Name = Name;
+            this.Content = Content;
+        }
+
+        public string getName()
+        {
+            return this.Name;
+        }
+
+        public string getType()
+        {
+            return "regular";
+        }
+
+        public string getContent()
+        {
+            return this.Content;
+        }
 
         public IList<Transition> Transitions { get; set; } = new List<Transition>();
 
         public Achievement Achievement { get; set; };
-
-        public string Content = "";
     }
 }
