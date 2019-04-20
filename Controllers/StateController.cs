319f00743be0bde84e3df6ac80df20456cee0cd1
Alexander Makarov <sam@rmcreative.ru>
2019-04-20 15:09:45 +0300

    Renamed endpoints

---
+++ Controllers/StateController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace simulator_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StateController : ControllerBase
+    {
+        // GET api/state/current
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/state/transit
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
